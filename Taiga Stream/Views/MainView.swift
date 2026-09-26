c10e79d1ca8045feb157b61d39757eade1f0af1a
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-09-26 17:07:17 +0200

    Version update

--- Taiga Stream/Views/MainView.swift
+++ Taiga Stream/Views/MainView.swift
@@ -43,6 +43,10 @@ struct MainView: View {
 		.listStyle(.plain)
 		.scrollContentBackground(.hidden)
 		.environment(\.defaultMinListRowHeight, 0)
+		.modifier(HardTopScrollEdge())
+		.toolbar {
+			TitleBarToolbar()
+		}
 	}
 
 	private var edgeSpacer: some View {
@@ -53,5 +57,33 @@ struct MainView: View {
 			.moveDisabled(true)
 			.deleteDisabled(true)
 			.accessibilityHidden(true)
+	}
+}
+
+private struct TitleBarToolbar: ToolbarContent {
+	@ToolbarContentBuilder
+	var body: some ToolbarContent {
+		if #available(macOS 26.0, *) {
+			ToolbarItem(placement: .navigation) {
+				Image(systemName: "chevron.left")
+					.hidden()
+			}
+			.sharedBackgroundVisibility(.hidden)
+		} else {
+			ToolbarItem(placement: .navigation) {
+				Image(systemName: "chevron.left")
+					.hidden()
+			}
+		}
+	}
+}
+
+private struct HardTopScrollEdge: ViewModifier {
+	func body(content: Content) -> some View {
+		if #available(macOS 26.0, *) {
+			content.scrollEdgeEffectStyle(.hard, for: .top)
+		} else {
+			content
+		}
 	}
 }
